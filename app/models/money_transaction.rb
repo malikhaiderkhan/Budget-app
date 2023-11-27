c1d5aeb7aacbc94af322f467756729f1c2b761d1
Haider <malikhaiderkhan81@gmail.com>
2023-11-28 01:22:06 +0500

    add validations

--- app/models/money_transaction.rb
+++ app/models/money_transaction.rb
@@ -1,4 +1,6 @@
 class MoneyTransaction < ApplicationRecord
+  validates :name, presence: true
+  validates :amount, presence: true
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   has_many :category_transactions
   has_many :categories, through: :category_transactions
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, presence: true
   has_many :categories
   has_many :money_transactions
   devise :database_authenticatable, :registerable,
