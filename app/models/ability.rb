214d2baabb55e4e63377f347473a64981e71e525
Haider <malikhaiderkhan81@gmail.com>
2023-11-28 21:44:56 +0500

    add authorization

---
+++ app/models/ability.rb
@@ -0,0 +1,14 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new  # Guest user
+
+    # if user.admin?
+      can :manage, :all
+    # else
+      can :read, :all
+      # Define other abilities based on user roles
+    # end
+  end
+end
