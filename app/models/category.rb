e4a7c8da8628c466d3f36bed643a0e55ac60ce59
Haider <malikhaiderkhan81@gmail.com>
2023-12-01 02:27:00 +0500

    style index page of category

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,5 @@ class Category < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :category_transactions
   has_many :money_transactions, through: :category_transactions
+  has_one_attached :icon
 end
