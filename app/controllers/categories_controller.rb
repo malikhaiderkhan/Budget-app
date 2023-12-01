2942983dbb7f712989f0a8f647e853c8d2b31326
Haider <malikhaiderkhan81@gmail.com>
2023-12-01 13:11:22 +0500

    style the categories pages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,7 +11,7 @@ class CategoriesController < ApplicationController
 
   # GET /categories/new
   def new
-    @category = Category.new
+    @category = current_user.categories.new
   end
 
   # GET /categories/1/edit
@@ -19,7 +19,7 @@ class CategoriesController < ApplicationController
 
   # POST /categories or /categories.json
   def create
-    @category = current_user.categories.new(category_params)
+    @category = current_user.categories.build(category_params)
 
     respond_to do |format|
       if @category.save
@@ -64,6 +64,6 @@ class CategoriesController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def category_params
-    params.require(:category).permit(:name, :icon)
+    params.require(:category).permit(:name, :icon, :user_id)
   end
 end
