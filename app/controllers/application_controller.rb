3c4893d8ca9abf6710a626d888132296e17fc97a
Haider <malikhaiderkhan81@gmail.com>
2023-12-02 02:33:41 +0500

    fix offenses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, unless: -> { controller_name == 'home' && action_name == 'index' }
-  
+
   def after_sign_in_path_for(_resource)
     categories_path
   end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,13 +2,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new  # Guest user
+    user || User.new # Guest user
 
     # if user.admin?
-      can :manage, :all
+    can :manage, :all
     # else
-      can :read, :all
-      # Define other abilities based on user roles
+    can :read, :all
+    # Define other abilities based on user roles
     # end
   end
 end
