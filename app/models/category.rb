a77d8700d6bc16559ea9e9f2d72329fd63c34a74
Haider <malikhaiderkhan81@gmail.com>
2024-02-18 01:15:54 +0500

    implement all changes required

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
   validates :name, :icon, presence: true
   belongs_to :user
-  has_many :payments, dependent: :destroy
+  has_many :category_payments
+  has_many :payments, through: :category_payments
   has_one_attached :icon
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_17_094436) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_17_183213) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_17_094436) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "category_payments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "payments", force: :cascade do |t|
